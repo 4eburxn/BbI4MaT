c9f843e779702901c0f87a93e85e00f41424c99a
Gousev Ivan <vanyagousev@gmail.com>
2025-10-17 13:18:58 +0300

    initial commit

---
+++ lib/norm.hpp
@@ -0,0 +1,42 @@
+#ifndef __NORM_HPP__
+#define __NORM_HPP__
+#include <cmath>
+#include <xtensor-blas/xlinalg.hpp>
+#include <xtensor/containers/xarray.hpp>
+#include <xtensor/io/xio.hpp>
+#include <xtensor/views/xview.hpp>
+
+template <double N, class T> auto normV(xt::xarray<T> tensor) {
+  using namespace xt;
+  static_assert(N > 0 || N == -1., "normM: norm must be > 0 OR -1");
+
+  if constexpr (N == 1.)
+    return sum(abs(tensor));
+  if constexpr (N == -1.)
+    return amax(tensor);
+  return pow(sum(pow(abs(tensor), N)), 1 / N);
+}
+template <int N, class T> auto normV(xt::xarray<T> tensor) {
+  using namespace xt;
+  static_assert(N > 0 || N == -1, "normM: norm must be > 0 OR -1");
+
+  if constexpr (N == 1)
+    return sum(abs(tensor));
+  if constexpr (N == -1)
+    return amax(tensor);
+  return pow(sum(pow(abs(tensor), (double)N)), 1 / (double)N);
+}
+
+template <int N, class T> auto normM(xt::xarray<T> tensor) {
+  using namespace xt;
+  static_assert(-2 < N && N < 3,
+                "normM: norms, others than -1,1,2 are not implemented");
+
+  if constexpr (N == 1)
+    return amax(sum(abs(tensor), {1}));
+  if constexpr (N == -1)
+    return amax(sum(abs(tensor), {0}));
+  return amax(diag(std::get<1>(xt::linalg::svd(tensor))));
+}
+
+#endif // __NORM_HPP__
---
+++ lib/solver.hpp
@@ -0,0 +1,9 @@
+#ifndef __SOLVER_HPP__
+#define __SOLVER_HPP__
+#include <cmath>
+#include <xtensor-blas/xlinalg.hpp>
+#include <xtensor/containers/xarray.hpp>
+#include <xtensor/io/xio.hpp>
+#include <xtensor/views/xview.hpp>
+
+#endif // __SOLVER_HPP__
