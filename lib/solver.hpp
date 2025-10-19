62efd44f5d2fc5f2e96916a8703fc67990a318a5
Gousev Ivan <vanyagosev@gmail.com>
2025-10-19 21:11:55 +0300

    unordered gaus

--- lib/solver.hpp
+++ lib/solver.hpp
@@ -1,9 +1,71 @@
 #ifndef __SOLVER_HPP__
 #define __SOLVER_HPP__
+#include "xtensor/core/xmath.hpp"
+#include "xtensor/misc/xmanipulation.hpp"
+#include "xtensor/views/xslice.hpp"
 #include <cmath>
+#include <iostream>
 #include <xtensor-blas/xlinalg.hpp>
 #include <xtensor/containers/xarray.hpp>
 #include <xtensor/io/xio.hpp>
 #include <xtensor/views/xview.hpp>
+
+enum SolverType { UnorderedGauss, Gauss, Orthogonalization, tridiagonal };
+
+template <class T>
+inline xt::xarray<T> unordered_gauss_solver(xt::xarray<T> tensor) {
+  using namespace xt::placeholders;
+  int n = tensor.shape()[0];
+
+  for (int i = 0; i < n - 1; i++) {
+    auto view_ = xt::range(i, _);
+    auto v = xt::view(tensor, i, view_);
+    for (int j = i + 1; j < n; j++) {
+      auto vv = xt::view(tensor, j, view_);
+      vv -= (tensor(j, i) / tensor(i, i)) * v;
+    }
+  }
+  for (int i = n - 1; i != 0; i--) {
+    auto view_ = xt::range(i, _);
+    auto v = xt::view(tensor, i, view_);
+    for (int j = i - 1; j >= 0; j--) {
+      auto vv = xt::view(tensor, j, view_);
+      vv -= (tensor(j, i) / tensor(i, i)) * v;
+    }
+  }
+  return tensor;
+}
+
+template <class T> inline xt::xarray<T> gauss_solver(xt::xarray<T> tensor) {
+  using namespace xt::placeholders;
+  int n = tensor.shape()[0];
+
+  for (int i = 0; i < n - 1; i++) {
+    auto mxi = i;
+    auto mx = std::abs(tensor(i, i));
+    for (int j = i + 1; j < n; j++) {
+      if (std::abs(tensor(j, i)) > mx) {
+        mxi = j;
+        mx = std::abs(tensor(j, i));
+      }
+    }
+    // xt::swapaxes(tensor, mxi, i);
+    auto view_ = xt::range(i, _);
+    auto v = xt::view(tensor, i, view_);
+    for (int j = i + 1; j < n; j++) {
+      auto vv = xt::view(tensor, j, view_);
+      vv -= (tensor(j, i) / tensor(i, i)) * v;
+    }
+  }
+  // for (int i = n - 1; i != 0; i--) {
+  //   auto view_ = xt::range(i, _);
+  //   auto v = xt::view(tensor, i, view_);
+  //   for (int j = i - 1; j >= 0; j--) {
+  //     auto vv = xt::view(tensor, j, view_);
+  //     vv -= (tensor(j, i) / tensor(i, i)) * v;
+  //   }
+  // }
+  return tensor;
+}
 
 #endif // __SOLVER_HPP__
--- main.cpp
+++ main.cpp
@@ -1,17 +1,23 @@
 #include "lib/norm.hpp"
+#include "lib/solver.hpp"
 #include "xtensor/core/xmath.hpp"
 #include <iostream>
 #include <xtensor/containers/xarray.hpp>
 #include <xtensor/io/xio.hpp>
 #include <xtensor/views/xview.hpp>
 int main(int argc, char *argv[]) {
-  xt::xarray<double> arr1{{1.0, -3.0, 2.0}, {4.0, 5.0, -1.0}, {3.0, 8.0, -6.0}};
+  // xt::xarray<double> arr1{
+  //     {1.0, -3.0, 2.0, 1.}, {4.0, 5.0, -1.0, 2.}, {3.0, 8.0, -6.0, 3.}};
+  xt::xarray<double> arr1{
+      {1.0, 0, 0, 1.}, {4.0, 5.0, 0, 2.}, {3.0, 8.0, -6.0, 3.}};
 
   xt::xarray<double> arr2{-5.0, 6.0, 7};
 
-  xt::xarray<double> res = xt::view(arr1, 1) + arr2;
+  // xt::xarray<double> res = xt::view(arr1, 1) + arr2;
 
-  std::cout << normM<2>(arr1) << std::endl;
+  std::cout << arr1 << std::endl;
+  std::cout << unordered_gauss_solver(arr1) << std::endl;
+  std::cout << gauss_solver(arr1) << std::endl;
 
   return 0;
 }
