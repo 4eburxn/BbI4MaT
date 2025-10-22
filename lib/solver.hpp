591eac430de5dc98be1a3420adfab7f0038f77b8
Gousev Ivan <vanyagousev@gmail.com>
2025-10-23 00:49:51 +0300

    sync3

--- lib/solver.hpp
+++ lib/solver.hpp
@@ -6,20 +6,27 @@
 #include "xtensor/views/xslice.hpp"
 #include <cmath>
 #include <iostream>
+#include <omp.h>
 #include <xtensor-blas/xlinalg.hpp>
 #include <xtensor/containers/xarray.hpp>
 #include <xtensor/containers/xtensor.hpp>
 #include <xtensor/io/xio.hpp>
 #include <xtensor/views/xview.hpp>
 
-enum SolverType { UnorderedGauss, Gauss, Orthogonalization, tridiagonal };
+enum SolverType {
+  Default,
+  UnorderedGauss,
+  Gauss,
+  Orthogonalization,
+  tridiagonal
+};
 
 template <class T>
 inline xt::xarray<T> unordered_gauss_solver(xt::xarray<T> tensor) {
   using namespace xt::placeholders;
   int n = tensor.shape()[0];
 
-  for (int i = 0; i < n - 1; i++) {
+  for (int i = 0; i != n - 1; i++) {
     auto view_ = xt::range(i, _);
     auto v = xt::view(tensor, i, view_);
     for (int j = i + 1; j < n; j++) {
@@ -73,6 +80,81 @@ template <class T> inline xt::xarray<T> gauss_solver(xt::xarray<T> tensor) {
     }
   }
   return tensor;
+}
+
+template <class T>
+inline std::pair<xt::xarray<T>, xt::xarray<T>>
+lu_decomposition(const xt::xarray<T> &A) {
+  using namespace xt::placeholders;
+  size_t n = A.shape()[0];
+  xt::xarray<T> L = xt::eye<T>({n, n});
+  xt::xarray<T> U = xt::zeros<T>({n, n});
+
+  for (size_t i = 0; i < n; ++i) {
+    auto ppp = xt::range(_, i);
+    auto L_i = xt::view(L, i, ppp);
+    for (size_t j = i; j < n; ++j) {
+      auto U_col_j = xt::view(U, ppp, j);
+      U(i, j) = A(i, j) - xt::linalg::vdot(L_i, U_col_j);
+    }
+
+    T U_diag = U(i, i);
+    auto vvv = xt::range(i + 1, _);
+    auto L_col_i = xt::view(L, vvv, i);
+    auto A_col_i = xt::view(A, vvv, i);
+
+    for (size_t k = i + 1; k < n; ++k) {
+      auto L_k = xt::view(L, k, ppp);
+      auto U_col_i = xt::view(U, ppp, i);
+      L(k, i) = (A(k, i) - xt::linalg::vdot(L_k, U_col_i)) / U_diag;
+    }
+  }
+  return {L, U};
+}
+
+template <class T>
+inline xt::xarray<T> solver(xt::xarray<T> tensor, xt::xarray<T> b) {
+  using namespace xt;
+  using namespace xt::placeholders;
+  size_t n = tensor.shape()[0];
+  xt::xarray<T> L = xt::eye<T>({n, n});
+  xt::xarray<T> U = xt::zeros<T>({n, n});
+
+  for (size_t i = 0; i < n; ++i) {
+    auto ppp = xt::range(_, i);
+    auto L_i = xt::view(L, i, ppp);
+    for (size_t j = i; j < n; ++j) {
+      auto U_col_j = xt::view(U, ppp, j);
+      U(i, j) = tensor(i, j) - xt::linalg::vdot(L_i, U_col_j);
+    }
+
+    T U_diag = U(i, i);
+    auto vvv = xt::range(i + 1, _);
+    auto L_col_i = xt::view(L, vvv, i);
+    auto A_col_i = xt::view(tensor, vvv, i);
+
+    for (size_t k = i + 1; k < n; ++k) {
+      auto L_k = xt::view(L, k, ppp);
+      auto U_col_i = xt::view(U, ppp, i);
+      L(k, i) = (tensor(k, i) - xt::linalg::vdot(L_k, U_col_i)) / U_diag;
+    }
+  }
+
+  for (int i = 0; i != n - 1; i++) {
+    b(i) /= L(i, i);
+    // auto V = xt::view(b, xt::range(i + 1, n - 1));
+    // V -= V * view(L, xt::range(i + 1, n - 1), i);
+    for (int j = i + 1; j < n; j++) {
+      b(j) -= b(i) * (L(j, i));
+    }
+  }
+  for (int i = n - 1; i != 0; i--) {
+    b(i) /= U(i, i);
+    for (int j = i - 1; j >= 0; j--) {
+      b(j) -= b(i) * (U(j, i));
+    }
+  }
+  return b;
 }
 
 #endif // __SOLVER_HPP__
--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
+// #define XTENSOR_USE_XSIMD 1
+// #define XTENSOR_USE_OPENMP 1
 #include "lib/norm.hpp"
 #include "lib/solver.hpp"
+#include "xtensor-blas/xlinalg.hpp"
 #include "xtensor/core/xmath.hpp"
+#include "xtensor/core/xtensor_forward.hpp"
+#include "xtensor/misc/xmanipulation.hpp"
+#include "xtensor/views/xstrided_view.hpp"
 #include <chrono>
 #include <cstdlib>
 #include <iostream>
@@ -9,22 +15,28 @@
 #include <xtensor/io/xio.hpp>
 #include <xtensor/views/xview.hpp>
 int main(int argc, char *argv[]) {
-  // xt::xarray<double> arr1{
-  //     {1.0, -3.0, 2.0, 1.}, {4.0, 5.0, -1.0, 2.}, {3.0, 8.0, -6.0, 3.}};
-  xt::random::seed(227);
-  xt::xarray<double> m = xt::random::randn<double>({10000, 10001});
+  using namespace xt::placeholders;
   xt::xarray<double> arr1{
-      {1.0, 0, 0, 1.}, {4.0, 5.0, 0, 2.}, {3.0, 8.0, -6.0, 3.}};
+      {1.0, -3.0, 2.0, 1.}, {4.0, 5.0, -1.0, 2.}, {3.0, 8.0, -6.0, 3.}};
+  xt::random::seed(227);
+  xt::xarray<double> m = xt::random::randn<double>({2000, 2000});
+  xt::xarray<double> b = xt::random::randn<double>({2000});
+  // xt::xarray<double> arr1{
+  //     {1.0, 0, 0, 1.}, {4.0, 5.0, 0, 2.}, {3.0, 8.0, -6.0, 3.}};
 
-  xt::xarray<double> arr2{-5.0, 6.0, 7};
+  xt::xarray<double> arr2{1, 2, 3};
+  xt::xarray<double> arr3{4, 5, 6};
 
   // xt::xarray<double> res = xt::view(arr1, 1) + arr2;
 
+  std::cout << xt::linalg::vdot(xt::view(arr2, xt::all()), arr3) << std::endl;
   std::cout << arr1 << std::endl;
-  std::cout << unordered_gauss_solver(arr1) << std::endl;
-  // std::cout << gauss_solver(m) << std::endl;
+  std::cout << solver(arr1, arr2) << std::endl;
+  auto LU = lu_decomposition(arr1);
+  std::cout << LU.first << "\n" << LU.second << std::endl;
   auto start = std::chrono::high_resolution_clock::now();
-  auto p = gauss_solver(m);
+  // auto p = lu_decomposition(m);
+  auto p = solver(m, b);
   auto finish = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double, std::milli> elapsed = finish - start;
   std::cout << "Elapsed Time: " << elapsed.count() << " milseconds"
